271028aaf9f8d129fc62df2d32fdb0a50ae02466
arnaud <arnaud.schaer@orange.com>
2017-08-30 13:15:06 +0200

    update tag 0.3.0 for 5.5.3 compatibility

--- public/tagcloud.js
+++ public/tagcloud.js
@@ -2,6 +2,10 @@ import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
 import { VisSchemasProvider } from 'ui/vis/schemas';
 
+// Include the angular controller
+require('plugins/tr-k4p-tagcloud/tagcloudController');
+require('plugins/tr-k4p-tagcloud/tagcloud.css');
+
 // THe provider function, which must return our new visualization type
 function TagcloudProvider(Private) {
 	var TemplateVisType = Private(TemplateVisTypeProvider);
